23411f00de9602b90ef3c16dc68e7091c42dfd51
McRata0 <133047333+McRata0@users.noreply.github.com>
2024-10-04 10:40:46 -0300

    Add files via upload

---
+++ catequese/perguntas.js
@@ -0,0 +1,70 @@
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual capital da frança?',
+    'Paris.'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quem escreveu "Dom Casmurro"?',
+    'Machado de Assis.'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual elemento químico com o símbolo O?',
+    'Oxigênio.'
+)
+
+criaCartao(
+    'Conhecimento gerais',
+    'Quem é o homem mais rico do mundo?',
+    'Jeff Bezos'
+)
+
+criaCartao(
+    'Conhecimento gerais',
+    'Qual montanha mais alta do mundo?',
+    'Monte Everest.'
+)
+
+criaCartao(
+    'Conhecimento gerais',
+    'Em que ano ocorreu a independência do Brasil?',
+    '1822.'
+)
+
+criaCartao(
+    'Conhecimento gerais',
+    'Ate Quantos pontos o volei de areia vai?',
+    '21.'
+)
+
+criaCartao(
+    'Conhecimento gerais',
+    'Quem pintou a Monalisa?',
+    'Leonardo da Vinci.'
+)
+
+criaCartao(
+    'Conhecimento gerais',
+    'Qual maior planeta do sistema solar?',
+    'Júpiter'
+)
+
+criaCartao(
+    'Conhecimento gerais',
+    'qual é a lingua mais falada no mundo?',
+    'Mandarim'
+)
+criaCartao(
+    'Conhecimento gerais',
+    'Quando jesus nasceu e morreu?',
+    'nasceu no ano 0 e morreu no ano 33 D.C .'
+)
+
+criaCartao(
+    'Conhecimento gerais',
+    'Quem foi o primeiro presidente dos Estado Unidos?',
+    'George Washington.'
+)
